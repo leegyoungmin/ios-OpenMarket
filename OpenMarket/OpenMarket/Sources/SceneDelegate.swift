254e2c6a141f3fb71208ff7ff70ce3ac60c05a28
미니 <cow970814@naver.com>
2022-12-12 18:27:47 +0900

    feat : 개인 프로젝트 초기 설정

---
+++ OpenMarket/OpenMarket/Sources/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  OpenMarket - SceneDelegate.swift
+//  Created by yagom. 
+//  Copyright © yagom. All rights reserved.
+// 
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let viewController = UINavigationController(rootViewController: ViewController())
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+    }
+}
+
