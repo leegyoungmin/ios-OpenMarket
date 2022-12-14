a445b7910f4d74ed604834d6636dad525109322a
미니 <cow970814@naver.com>
2022-12-14 17:56:33 +0900

    feat: OpenMarketViewController 세그먼트 생성

--- OpenMarket/OpenMarket/Resources/Controller/OpenMarketViewController/OpenMarketViewController.swift
+++ OpenMarket/OpenMarket/Resources/Controller/OpenMarketViewController/OpenMarketViewController.swift
@@ -7,12 +7,31 @@
 import UIKit
 
 class OpenMarketViewController: UIViewController {
+    let layoutSegment: UISegmentedControl = {
+        let segment = UISegmentedControl(items: ["LIST", "GRID"])
+        segment.selectedSegmentIndex = .zero
+        return segment
+    }()
+    
+    let collectionView: UICollectionView? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setUpNavigationBar()
+        setSegmentAction()
         view.backgroundColor = .systemBackground
+    }
+}
+
+// MARK: - UserInteraction
+extension OpenMarketViewController {
+    func setSegmentAction() {
+        layoutSegment.addTarget(self, action: #selector(didChangeSegmentIndex), for: .valueChanged)
+    }
+    
+    @objc func didChangeSegmentIndex(_ sender: UISegmentedControl) {
+        print(sender.selectedSegmentIndex)
     }
 }
 
@@ -22,6 +41,8 @@ extension OpenMarketViewController {
             self.dismiss(animated: true)
         }
         
+        navigationItem.titleView = layoutSegment
+        navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = AnyTargetAPI.OpenMarket.rawValue
         navigationItem.leftBarButtonItem = UIBarButtonItem(systemItem: .close, primaryAction: dismissAction)
     }
